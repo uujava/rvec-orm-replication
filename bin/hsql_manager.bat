b348dff3eb04c91c7937402987e6753f05839410
vkozyr <kozyr@programpark.ru>
2015-10-26 14:34:25 +0300

    fixed java path

--- bin/hsql_manager.bat
+++ bin/hsql_manager.bat
@@ -1,5 +1,5 @@
 setlocal
 call %~dp0env.bat
 set HSQL_PATH=%RVEC_HOME%\server\rvec_server\modules\ext\ru.programpark.rvec.common-deps\org-hsqldb\hsqldb.jar
-%JAVA_HOME%\bin\java -jar %HSQL_PATH%
+"%JAVA_HOME%\bin\java" -jar %HSQL_PATH%
 endlocal
